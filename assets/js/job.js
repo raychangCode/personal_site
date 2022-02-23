85954781fa1c97b069ef97e29f898b6ac2f253a3
raychangCode <rayray840924@gmail.com>
2022-02-22 23:14:55 -0600

    update job search

--- assets/js/job.js
+++ assets/js/job.js
@@ -7,12 +7,12 @@ let user = []
 searchInput.addEventListener('input', (e) => {
     const value = e.target.value.toLowerCase()
     users.forEach(user => {
-        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value)
+        const isVisible = user.name.toLowerCase().includes(value) || user.title.toLowerCase().includes(value)
         user.element.classList.toggle('hide', !isVisible)
     });
 })
 
-fetch('https://remotive.io/api/remote-jobs?limit=200')
+fetch('https://remotive.io/api/remote-jobs?limit=250')
     .then(res => res.json())
     .then(data => {
         data = data.jobs
@@ -20,12 +20,10 @@ fetch('https://remotive.io/api/remote-jobs?limit=200')
             const card = userCardTemplate.content.cloneNode(true).children[0]
             const header = card.querySelector('[data-header]')
             const body = card.querySelector('[data-body]')
-            const jobType = card.querySelector('[data-job-type]')
             header.textContent = user.company_name
             body.textContent = user.title
-            jobType.textContent = user.job_type
             userCardContainer.append(card)
-            return { name: user.company_name, email: user.title, job_type: user.job_type, element: card }
+            return { name: user.company_name, title: user.title, element: card }
         });
 
     })
