87b51d85a8e862af4577a602670bc93bf44d85df
raychangCode <rayray840924@gmail.com>
2022-02-25 07:40:02 -0600

    fix bug

--- assets/js/job.js
+++ assets/js/job.js
@@ -3,7 +3,6 @@ const userCardContainer = document.querySelector('[data-user-cards=container]')
 const searchInput = document.querySelector('[data-search]')
 
 let user = []
-getData()
 
 searchInput.addEventListener('input', (e) => {
     const value = e.target.value.toLowerCase()
@@ -15,34 +14,34 @@ searchInput.addEventListener('input', (e) => {
 
 
 // load data and create card
-function getData() {
 
-    fetch('https://remotive.io/api/remote-jobs?limit=200')
-        .then(res => res.json())
-        .then(data => {
-            data = data.jobs
-            users = data.map(user => {
 
-                const card = userCardTemplate.content.cloneNode(true).children[0]
-                const header = card.querySelector('[data-header]')
-                const body = card.querySelector('[data-body]')
-                const jobType = card.querySelector('[data-job-type]')
-                let link = document.createElement('a')
-                link.setAttribute('href', user.url)
-                link.setAttribute('class', 'fill-div')
-                link.textContent = 'Apply'
-                header.textContent = user.company_name
-                body.textContent = user.title
-                jobType.textContent = user.job_type
-                card.append(link)
-                userCardContainer.append(card)
-                return { name: user.company_name, email: user.title, job_type: user.job_type, element: card }
+fetch('https://remotive.io/api/remote-jobs')
+    .then(res => res.json())
+    .then(data => {
+        data = data.jobs
+        users = data.map(user => {
 
-            });
+            const card = userCardTemplate.content.cloneNode(true).children[0]
+            const header = card.querySelector('[data-header]')
+            const body = card.querySelector('[data-body]')
+            const jobType = card.querySelector('[data-job-type]')
+            let link = document.createElement('a')
+            link.setAttribute('href', user.url)
+            link.setAttribute('class', 'fill-div')
+            link.textContent = 'Apply'
+            header.textContent = user.company_name
+            body.textContent = user.title
+            jobType.textContent = user.job_type
+            card.append(link)
+            userCardContainer.append(card)
+            return { name: user.company_name, email: user.title, job_type: user.job_type, element: card }
 
-        })
+        });
 
-}
+    })
+
+
 
 // fetch('https://remotive.io/api/remote-jobs?limit=200')
 //     .then(res => res.json())
