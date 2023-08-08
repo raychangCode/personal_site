5b1e5a059bc225a36b41d1179a3f3edc3cc8a832
Jui-Ting Chang <73032749+raychangCode@users.noreply.github.com>
2023-08-09 07:22:21 +0800

    Update job.js
    remove markdown codes

--- assets/js/job.js
+++ assets/js/job.js
@@ -40,27 +40,3 @@ fetch('https://remotive.io/api/remote-jobs')
         });
 
     })
-
-
-
-// fetch('https://remotive.io/api/remote-jobs?limit=200')
-//     .then(res => res.json())
-//     .then(data => {
-//         data = data.jobs
-//         users = data.map(user => {
-
-//             const card = userCardTemplate.content.cloneNode(true).children[0]
-//             const header = card.querySelector('[data-header]')
-//             const body = card.querySelector('[data-body]')
-//             const jobType = card.querySelector('[data-job-type]')
-//             header.textContent = user.company_name
-//             body.textContent = user.title
-//             jobType.textContent = user.job_type
-
-//             userCardContainer.append(card)
-//             return { name: user.company_name, email: user.title, job_type: user.job_type, element: card }
-
-//         });
-
-//     })
-
